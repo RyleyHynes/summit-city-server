18ff54df5bfcdac42f70f364a7db7b04bb38fb95
Ryley Hynes <ryleyhynes@gmail.com>
2022-10-25 14:41:35 -0500

    need to fix bugs with my hikes and climbs

--- summitapi/models/my_climb.py
+++ summitapi/models/my_climb.py
@@ -5,8 +5,6 @@ from django.db import models
 
 class MyClimb(models.Model):
     # Step 2: Add any fields on the erd
-    completed = models.BooleanField(default=False)
-    bucket_list = models.BooleanField(default=False)
     summit_user = models.ForeignKey(
         "SummitUser", on_delete=models.CASCADE, related_name="list_climbs")
     climbs = models.ManyToManyField("Climb", related_name="climbs")
